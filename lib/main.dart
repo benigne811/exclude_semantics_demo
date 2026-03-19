5e71262f331758a87b49cda11206e32d6063155d
benigne811 <b.uwitonze@alustudent.com>
2026-03-19 23:01:47 +0200

    Final touches

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DemoApp());
+  runApp(const DemoApp());
 }
 
 class DemoApp extends StatelessWidget {
+  const DemoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
